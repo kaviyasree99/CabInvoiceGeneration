203d36ca84a8b9a9929cc638fcba85b746e260e6
kaviyasree99 <kaviyarajalingam@gmail.com>
2021-08-16 17:21:09 +0530

    Added: Adding Multiple Rides

--- src/test/java/InvoiceGeneratorTest.java
+++ src/test/java/InvoiceGeneratorTest.java
@@ -1,13 +1,11 @@
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.testng.Assert;
 
 public class InvoiceGeneratorTest {
-    private Assertions Assert;
-
     @Test
     public void givenDistanceAndTime_ShouldReturnTotalFare() {
-        InvoiceGenerator invoiceGenerator =new InvoiceGenerator();
+        InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
         double distance = 2.0;
         int time = 5;
         double fare = invoiceGenerator.calculateFare(distance, time);
@@ -17,9 +15,19 @@ public class InvoiceGeneratorTest {
     @Test
     public void givenLessDistanceAndTime_ShouldReturnMinFare() {
         InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
-        double distance =0.1;
+        double distance = 0.1;
         int time = 1;
         double fare = invoiceGenerator.calculateFare(distance, time);
         Assert.assertEquals(5, fare, 0.0);
+    }
+
+    @Test
+    public void givenMultipleRides_ShouldReturnTotalFare() {
+        InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
+        Ride[] rides = {new Ride(2.0, 5),
+                new Ride(0.1, 1)
+        };
+        double fare = invoiceGenerator.calculateFare(rides);
+        Assert.assertEquals(30, fare, 0.0);
     }
 }
